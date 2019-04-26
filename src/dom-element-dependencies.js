685126b92797dcf678d588666590c860e34ce954
Tom Berghuis <thomas.berghuis@gmail.com>
2019-04-26 14:58:17 +1000

    .

--- src/dom-element-dependencies.js
+++ src/dom-element-dependencies.js
@@ -1,4 +1,3 @@
-
 export default new Promise(resolve => {
   let player, watchFlexy;
   const assignDomElements = () => {
@@ -17,4 +16,3 @@ export default new Promise(resolve => {
     }
   };
 });
-
--- src/inject.js
+++ src/inject.js
@@ -2,13 +2,10 @@ import Vue from "vue";
 import App from "./components/App.vue";
 import domElementsPromise from "./dom-element-dependencies";
 
+// i could have done this in dom-element-dependencies
 async function overrideCalculateCurrentPlayerSize() {
   const { player, watchFlexy } = await domElementsPromise;
-  // console.log(
-  //   "TCL: overrideCalculateCurrentPlayerSize -> player, watchFlexy",
-  //   player,
-  //   watchFlexy
-  // );
+  watchFlexy.isTwoColumns_ = false;
   watchFlexy.calculateCurrentPlayerSize_ = function() {
     let width = player.clientWidth;
     let ratio = watchFlexy.videoHeightToWidthRatio_;
