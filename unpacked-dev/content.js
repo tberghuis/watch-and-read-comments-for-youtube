d7711da7ef16b937967d83fe870a5bf1f5465203
Thomas Berghuis <thomas.berghuis@gmail.com>
2022-07-30 17:28:17 +1000

    .

--- unpacked-dev/content.js
+++ unpacked-dev/content.js
@@ -1,35 +1,18 @@
-// var elemDiv = document.createElement("div");
-// elemDiv.id = "vue-app";
-// document.body.appendChild(elemDiv);
-
-// // version 6.6.3
-// inject("rxjs.js");
-// inject("inject.js", true);
-
-// /////////////////// functions
-
-// function inject(filename, isModule) {
-//   var s = document.createElement("script");
-//   s.src = chrome.runtime.getURL(filename);
-//   if (isModule) {
-//     s.type = "module";
-//   }
-
-//   (document.head || document.documentElement).appendChild(s);
-// }
-
-
-////////////////// start
-
 console.log("hello content");
 
 createVueRoot();
+
+// version 6.6.3
+inject("rxjs.js");
+inject("inject.js", true);
 
 // for release, built content.js does not need to be injected
 devInjectContent();
 
 ///////////////// functions
+
 function devInjectContent() {
+  console.log("REMOVE ME RELEASE VERSION, DEV ONLY");
   var s = document.createElement("script");
   s.src = "http://localhost:5173/src/main.js";
   s.type = "module";
@@ -40,4 +23,14 @@ function createVueRoot() {
   var elemDiv = document.createElement("div");
   elemDiv.id = "vue-app";
   document.body.appendChild(elemDiv);
+}
+
+function inject(filename, isModule) {
+  var s = document.createElement("script");
+  s.src = chrome.runtime.getURL(filename);
+  if (isModule) {
+    s.type = "module";
+  }
+
+  (document.head || document.documentElement).appendChild(s);
 }
