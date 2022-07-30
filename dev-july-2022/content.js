a00a595d35b97f1457fd1987a98792b2090072ff
Thomas Berghuis <thomas.berghuis@gmail.com>
2022-07-30 15:34:01 +1000

    .

--- dev-july-2022/content.js
+++ dev-july-2022/content.js
@@ -1,1 +1,16 @@
 console.log("hello content");
+
+inject();
+
+
+
+
+
+/////////////////// functions
+
+function inject() {
+  var s = document.createElement("script");
+  s.src = "http://127.0.0.1:5173/src/demo.ts";
+  s.type = "module";
+  document.head.appendChild(s);
+}
