f6f6dbe078fec0c75a243b92d18e2cb9e1226539
Thomas Berghuis <thomas.berghuis@gmail.com>
2022-11-03 14:57:39 +1100

    .

--- content-vite/src/components/StaticStyles.vue
+++ content-vite/src/components/StaticStyles.vue
@@ -49,6 +49,10 @@
     ytd-expander.ytd-video-secondary-info-renderer {
       --ytd-expander-collapsed-height: '' !important;
     }
+    #top-row {
+      display: block !important;
+    }
+
   </v-style>
 </template>
 
